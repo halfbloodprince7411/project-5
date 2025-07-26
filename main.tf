57343538b0b56b82f07ce765386f04806e3ca2aa
siddharth7411 <siddharth7411@gmail.com>
2025-07-26 19:15:39 -0400

    reborn

--- main.tf
+++ main.tf
@@ -9,35 +9,35 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-severussnape"
+  name     = "rg-death-eaters-${random_pet.name.id}"
   location = "West US"
 }
 
-resource "azurerm_app_service_plan" "plan" {
-  name                = "asp-lightweight"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
+# resource "azurerm_app_service_plan" "plan" {
+#   name                = "asp-lightweight"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   kind                = "Linux"
+#   reserved            = true
 
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-}
+#   sku {
+#     tier = "Basic"
+#     size = "B1"
+#   }
+# }
 
-resource "azurerm_app_service" "app" {
-  name                = "lightweight-app-${random_pet.name.id}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+# resource "azurerm_app_service" "app" {
+#   name                = "lightweight-app-${random_pet.name.id}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   app_service_plan_id = azurerm_app_service_plan.plan.id
 
-  site_config {
-    linux_fx_version = "NODE|14-lts" # Feel free to change this to your runtime
-  }
+#   site_config {
+#     linux_fx_version = "NODE|14-lts" # Feel free to change this to your runtime
+#   }
 
-  https_only = true
-}
+#   https_only = true
+# }
 
 resource "random_pet" "name" {
   length    = 2
