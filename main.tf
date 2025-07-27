2b768e867fe27a0ae7385e3cfcfa6bfbefc8882d
siddharth7411 <siddharth7411@gmail.com>
2025-07-26 22:14:44 -0400

    reborn9.0

--- main.tf
+++ main.tf
@@ -82,8 +82,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22_04-lts"
-    version   = "latest"
+    sku       = "22_04-lts-gen2"
+    version   = "22.04.202212010"
   }
 
   tags = {
