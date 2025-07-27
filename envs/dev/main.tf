fb2a3b0df3f4b54095324d244ac6d5807e1d8643
siddharth7411 <siddharth7411@gmail.com>
2025-07-27 01:44:07 -0400

    rebornagain 15.0

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -13,14 +13,14 @@ module "vnet" {
   source              = "../../modules/vnet"
   name                = var.vnet_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = var.address_space
 }
 
 # Example: Subnet using vnet module output
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-${module.vnet.vnet_name}"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = module.vnet.vnet_name
   address_prefixes     = ["10.0.1.0/24"]
 }
