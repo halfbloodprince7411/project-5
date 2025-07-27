24ee7f27acb54239e03749477e276dd23d787155
siddharth7411 <siddharth7411@gmail.com>
2025-07-26 22:17:18 -0400

    reborn10.0

--- main.tf
+++ main.tf
@@ -81,7 +81,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts-gen2"
     version   = "22.04.202212010"
   }
