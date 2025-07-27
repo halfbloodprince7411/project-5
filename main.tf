7e6ccc33e2a5645d7644a72c42f30c0fcd504639
siddharth7411 <siddharth7411@gmail.com>
2025-07-26 20:03:01 -0400

    reborn1.0

--- main.tf
+++ main.tf
@@ -7,39 +7,88 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+# 🐾 Generate spooky pet-style names
+resource "random_pet" "name" {
+  length    = 2
+  separator = "-"
+}
 
+# ☠️ Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "rg-death-eaters-${random_pet.name.id}"
   location = "West US"
 }
 
-# resource "azurerm_app_service_plan" "plan" {
-#   name                = "asp-lightweight"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   kind                = "Linux"
-#   reserved            = true
+# 🌐 Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${random_pet.name.id}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
 
-#   sku {
-#     tier = "Basic"
-#     size = "B1"
-#   }
-# }
+# 🧵 Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${random_pet.name.id}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-# resource "azurerm_app_service" "app" {
-#   name                = "lightweight-app-${random_pet.name.id}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   app_service_plan_id = azurerm_app_service_plan.plan.id
+# 🌎 Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "pip-${random_pet.name.id}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
 
-#   site_config {
-#     linux_fx_version = "NODE|14-lts" # Feel free to change this to your runtime
-#   }
+# 🔌 Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${random_pet.name.id}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   https_only = true
-# }
+  ip_configuration {
+    name                          = "ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
 
-resource "random_pet" "name" {
-  length    = 2
-  separator = "-"
+# 💻 Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-${random_pet.name.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s" # ✅ ultra-budget tier
+
+  admin_username      = "ubuntu"
+  admin_ssh_key {
+    username   = "ubuntu"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "dev"
+    owner       = "sirisha"
+    cost        = "minimal"
+    name        = random_pet.name.id
+  }
 }
